3465698d3ea49c4d1cad084951e2974ff1fb1cf3
Rahul Bishnoi <bishnoirahul107@gmail.com>
2026-06-16 11:05:47 +0530

    week 3 day 3 - get and post api

--- drf_project/api/views.py
+++ drf_project/api/views.py
@@ -3,10 +3,19 @@ from rest_framework.decorators import api_view
 from .models import Student
 from .serializers import StudentSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def home(request):
 
-    students = Student.objects.all()
-    serializer = StudentSerializer(students, many=True)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors)
